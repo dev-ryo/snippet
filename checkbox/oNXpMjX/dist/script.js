38014c5784aeaa2fac29d74d70fbddaa0843394b
RYO <42329240+dev-ryo@users.noreply.github.com>
2020-03-10 18:28:57 +0900

    update

--- checkbox/oNXpMjX/dist/script.js
+++ checkbox/oNXpMjX/dist/script.js
@@ -3,4 +3,9 @@ function switchCheck(elem) {
   else {elem.setAttribute('aria-checked', true);}
 }
 
-function checkWithEnter(elem) {if(event.keyCode === 32) switchCheck(elem);}
+function checkWithEnter(elem) {
+  if(event.keyCode === 32) {
+    switchCheck(elem);
+    event.preventDefault();
+  }
+}
